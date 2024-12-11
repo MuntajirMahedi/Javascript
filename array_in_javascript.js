6d28c6bd7d4dad00224aaa1254d0f56abee477e6
MuntajirMahedi <muntajirmahedi477@gmail.com>
2024-12-11 17:43:07 +0530

    add array

--- array_in_javascript.js
+++ array_in_javascript.js
@@ -1,20 +1,41 @@
-//console.log(2 > 2);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+//Array
 
-console.log("2" > 1);
-console.log("02" > 1);
+var myArr = [0, 1, 2, 3, 4, true,"rahul"];
+var myHeros = ["shakiman","naagraj"];
+var myArr2 = new Array(1, 2, 3, 4);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(myArr[5]);
+// console.log(myArr2[3]);
 
+//Array Methods
 
+// myArr.push(5);
+// myArr.push(6);
+// myArr.pop();
 
+// myArr.unshift(true);
+// myArr.shift();
 
+// console.log(myArr.includes('rahul'));
+// console.log(myArr.indexOf(true));
 
+var newArr = myArr.join();
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+console.log("A--", myArr);
+
+var myn1 = myArr.slice(1, 3);
+console.log(myn1);
+
+console.log("B--",myArr);
+
+var myn2 = myArr.splice(1, 3);
+
+console.log("C--",myArr);
+console.log(myn2);
 
 
 
