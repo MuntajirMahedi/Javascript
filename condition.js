13774ef9c942efd87a70a3ca183d04b80d88dc66
MuntajirMahedi <muntajirmahedi477@gmail.com>
2024-12-31 16:59:38 +0530

    if/else condition

---
+++ condition.js
@@ -0,0 +1,40 @@
+
+var a = 18;
+// console.log(a);
+
+if ((a > 0) && (a <= 17)) {
+    // console.log("you are a kid");
+}
+else if(a >= 18){
+    // console.log("you are 18 years old");
+}
+else{
+    // console.log("not valid");
+}
+
+//_______________________________________________________________
+
+//Write an if/else statement that checks if a number is even or odd.
+
+var number = 21;
+// console.log(number);
+
+if(number % 2 == 0){
+    // console.log("Even number");
+}
+else{
+    // console.log("Odd number");
+}
+
+//_______________________________________________________________
+
+//tarnary operator
+ 
+var number = 21;
+// console.log(number);
+// console.log("Odd Or Even Number : ", number % 2 == 0 ? "Even Number" : "Odd Number");
+
+
+//_______________________________________________________________
+
+
